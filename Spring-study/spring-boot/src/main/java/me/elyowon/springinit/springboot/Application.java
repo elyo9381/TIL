d047faddca0a12de9e459293b33880976a7f8350
elyo9381 <won4413@daum.net>
2020-07-13 16:01:16 +0900

    stringBoot InnerServer ConfigurationExam(change port,Disabling server)

--- Spring-study/spring-boot/src/main/java/me/elyowon/springinit/springboot/Application.java
+++ Spring-study/spring-boot/src/main/java/me/elyowon/springinit/springboot/Application.java
@@ -1,24 +1,34 @@
 package me.elyowon.springinit.springboot;
 
-import me.elyowon.Holoman;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        SpringApplication application = new SpringApplication(Application.class);
-        application.setWebApplicationType(WebApplicationType.NONE);
-        application.run(args);
+//        Tomcat tomcat =new Tomcat();
+//        tomcat.setPort(8080);
+//
+//        Context context = tomcat.addContext("","/");
+//
+//        HttpServlet servlet = new HttpServlet() {
+//            @Override
+//            protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//                PrintWriter writer = resp.getWriter();
+//                writer.println("<html><head><title>");
+//                writer.println("Hey, Tomcat");
+//                writer.println("</title></head>");
+//                writer.println("<body><h1>Hello TomCat</h1></body>");
+//                writer.println("</html>");
+//            }
+//        };
+//
+//        String servletName = "helloServlet";
+//        tomcat.addServlet("",servletName,servlet);
+//        context.addServletMappingDecoded("/hello",servletName);
+//
+//        tomcat.getConnector();
+//        tomcat.start();
+        SpringApplication.run(Application.class, args);
     }
-
-//    @Bean
-//    public Holoman holoman(){
-//        Holoman holoman = new Holoman();
-//        holoman.setName("elyowon white!~");
-//        holoman.setHowLong(60);
-//        return holoman;
-//    }
 }
