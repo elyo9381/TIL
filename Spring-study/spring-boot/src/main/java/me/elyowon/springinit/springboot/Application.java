eef389ca690a111aba4255963c5bdeed0d789814
elyo9381 <won4413@daum.net>
2020-07-14 20:16:51 +0900

    springboot properties, logger, properties priority

--- Spring-study/spring-boot/src/main/java/me/elyowon/springinit/springboot/Application.java
+++ Spring-study/spring-boot/src/main/java/me/elyowon/springinit/springboot/Application.java
@@ -3,14 +3,14 @@ package me.elyowon.springinit.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(ElyoProperties.class)
 public class Application {
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(Application.class);
-        app.setWebApplicationType(WebApplicationType.);
+        app.setWebApplicationType(WebApplicationType.NONE);
         app.run(args);
     }
 }
