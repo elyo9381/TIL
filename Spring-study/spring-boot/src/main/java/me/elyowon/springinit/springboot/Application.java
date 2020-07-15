b9e9dc36deef09df4f2c63d7342b7318c0623cb4
elyo9381 <won4413@daum.net>
2020-07-15 15:36:17 +0900

    springboot webMVC - HttpMessageConverters, ViewResolver

--- Spring-study/spring-boot/src/main/java/me/elyowon/springinit/springboot/Application.java
+++ Spring-study/spring-boot/src/main/java/me/elyowon/springinit/springboot/Application.java
@@ -1,16 +1,12 @@
 package me.elyowon.springinit.springboot;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(ElyoProperties.class)
 public class Application {
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(Application.class);
-        app.setWebApplicationType(WebApplicationType.NONE);
         app.run(args);
     }
 }
---
+++ Spring-study/spring-boot/src/main/java/me/elyowon/springinit/springboot/user/UserController.java
@@ -0,0 +1,16 @@
+package me.elyowon.springinit.springboot.user;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class UserController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @PostMapping("/users/create")
+    public User create(@RequestBody User user){
+        return user;
+    }
+}
