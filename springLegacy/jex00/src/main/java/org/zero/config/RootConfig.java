242a8241aff81c3c58dd8441724da771438d4b3f
elyo9381 <won4413@daum.net>
2020-07-27 23:57:06 +0900

    spring legacy prj init

---
+++ springLegacy/jex00/src/main/java/org/zero/config/RootConfig.java
@@ -0,0 +1,11 @@
+package org.zero.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"org.zero.sample"})
+public class RootConfig {
+	
+
+}
---
+++ springLegacy/jex00/src/main/java/org/zero/sample/Restaurant.java
@@ -0,0 +1,17 @@
+package org.zero.sample;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+import lombok.Setter;
+
+@Component
+@Data
+public class Restaurant {
+	
+	
+	@Setter(onMethod = @__({@Autowired}))
+	private Chef chef;
+
+}
