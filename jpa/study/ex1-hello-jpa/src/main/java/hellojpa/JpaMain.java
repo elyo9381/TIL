274dc9f35e1eaee33756be2b18932dd2a42e1ae7
elyo9381 <won4413@daum.net>
2021-01-22 22:19:02 +0900

    jpa-concept-2, managed persist&detached ,flush, transactional write-behind

--- jpa/study/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ jpa/study/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -21,6 +21,7 @@ public class JpaMain {
         tx.begin();
 
         try {
+            // 1,2번 jpa 소개, 시작
             //생성
 //            Member member = new Member();
 //            member.setId(2L);
@@ -38,12 +39,59 @@ public class JpaMain {
 
             // jpql >> 현업에서 개발의 고민은 : 데이터를 정말 최적화 해서 가져와야하고 통계성 쿼리도 날려야하는데
             // 이것을 jpql이 도와준다.
-            List<Member> result = em.createQuery("select m from Member as m",Member.class)
-                    .getResultList();
+//            List<Member> result = em.createQuery("select m from Member as m",Member.class)
+//                    .getResultList();
+//
+//            for(Member member : result){
+//                System.out.println("member.name = "+member.getName());
+//            }
 
-            for(Member member : result){
-                System.out.println("member.name = "+member.getName());
-            }
+            // 3번 강의 영속성관리
+            // 비영속
+//            Member member  = new Member();
+//            member.setName("HelloJPA");
+//            member.setId(101L);
+//
+//            //영속
+//            System.out.println("===before===") ;
+//            em.persist(member);
+//            System.out.println("===after===") ;
+//            Member findMember = em.find(Member.class,101L);
+//            System.out.println("findMember.id: " +findMember.getId());
+
+            //아래와 같이 조회시 첫 findMember1은 쿼리를 날리지만 2은 쿼리를 날리지 않고 1차캐시에서 가져온다.
+//            Member findMember1 = em.find(Member.class,101L);
+//            Member findMember1 = em.find(Member.class,101L);
+
+//
+//            // 바로 쿼리를 날리지 않고 영속 컨테스트 내부에 저장후 커밋이 나오면 쿼리를 날림
+//            Member member1 = new Member(150L,"A");
+//            Member member2 = new Member(160L,"B");
+//
+//            //  why? 버퍼링 쓰기위해 >> 버퍼링은 최적화를 하기 위한 개념 >> jdbc.batch를 사용하게 됨
+//            // 실시간 쿼리에서는 이점이 없을수도 있지만 기본적으로 이점을 가져가기 때문에 좋다.
+//            em.persist(member1);
+//            em.persist(member2);
+//
+//            System.out.println("-==============-");
+
+//            Member member = em.find(Member.class,150L);
+//            member.setName("ZZZZZ");
+//            // em.persist(member); 하지 않는 이유는 자바 컬렉션 처럼 셋네임 하면 자동으로 저장된다.
+//            // 이게 어떻게 내부적으로 지원되냐? >>  변경감지(dirty checking) 기능이 존재한다. >>  영속 컨테스트 내부적으로 커밋하면
+//            // 플러시가 작동하고  >> 1차캐시안에 엔티티와 스냅샷(값을 읽어온 시점)을 비교하여 다르다면 업데이트 쿼리를 쓰기지연 sql저장소에 작성한다.
+//            // 이러한 매커니즘을 통해서 진행된다.
+
+//             엔티티 삭제
+//                Member memberA = em.find(Member.class,"memberA");
+//                em.remove(memberA);
+
+            // 플러시
+
+            // 준영속 상태들어가는 방법
+
+
+
 
 
             tx.commit();
--- jpa/study/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ jpa/study/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -4,12 +4,22 @@ package hellojpa;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
-@Entity
+@Entity // table을 놓는다.
 public class Member {
 
     @Id
     private Long id;
     private String name;
+
+    //jpa는 생성자와 기본 생성자를 생성해줘야한다.
+    public Member() {
+    }
+
+    public Member(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
 
     public Long getId() {
         return id;
