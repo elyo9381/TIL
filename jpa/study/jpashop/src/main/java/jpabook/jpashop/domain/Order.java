6249b7c718615424123c2efb56a5aa68c054eee6
elyo9381 <won4413@daum.net>
2021-01-24 13:20:00 +0900

    jpa-concept-3 make requirement from UML to jpa

--- jpa/study/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpa/study/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -1,18 +1,54 @@
 package jpabook.jpashop.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.Date;
 
 @Entity
+@Table(name = "ORDERS") // ORDER는 DB에서 예약어로 걸린상황이 많음
 public class Order {
 
     @Id @GeneratedValue()
+    @Column(name = "ORDER_ID")
     private Long id;
 
+    @Column(name = "MEMBER_ID")
     private Long memberId;
+
     private LocalDateTime orderDate;
+
+    @Enumerated(EnumType.STRING)
     private OrderStatus status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(Long memberId) {
+        this.memberId = memberId;
+    }
+
+    public LocalDateTime getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDateTime orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
 }
