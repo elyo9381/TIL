e849277a6d1118b25decb1661f58732fe9fda9d7
elyo9381 <won4413@daum.net>
2023-03-08 21:27:22 +0900

    Updating and removing program source files, therefore this repository only creates md files.

--- JAVA/src/me/elyowon/Middle/javaUtil/exam/Compare.java
+++
@@ -1,6 +0,0 @@
-package me.elyowon.Middle.javaUtil.exam;
-
-
-public interface Compare {
-    public int compareTo(int value1, int value2);
-}
--- Spring-whiteship/spring-boot/src/test/java/me/elyowon/springinit/springboot/ApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package me.elyowon.springinit.springboot;
-
-import org.junit.jupiter.api.Test;
-
-class ApplicationTests {
-    @Test
-    void contextLoads() {
-    }
-}
